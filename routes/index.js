b5dbb547ffbd87441d4bcdae12380e7389781eeb
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-04-14 09:26:37 +0700

    template

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.sendFile("index.html")
+  res.render("index");
+});
+// GET 
+router.get('/about', function(req, res, next) {
+  res.render("about");
+});
+
+// GET 
+router.get('/blog', function(req, res, next) {
+  res.render("blog");
+});
+
+// GET 
+router.get('/contact', function(req, res, next) {
+  res.render("contact");
+});
+// GET 
+router.get('/services', function(req, res, next) {
+  res.render("services");
+});
+
+// GET 
+router.get('/single', function(req, res, next) {
+  res.render("single");
+});
+
+// GET 
+router.get('/collection', function(req, res, next) {
+  res.render("collection");
 });
 
 module.exports = router;
